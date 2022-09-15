82e51fdc66ffc3fbb9f7fec4607328e3ce457059
ameedf <ameed.software@gmail.com>
2022-09-15 19:57:10 +0300

    Removing unused imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import User from './components/User/User';
 import UsersList from './components/UsersList/UsersList';
 
 function App() {
